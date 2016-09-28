6d1e967f22d894928025077fb2a7830b7dcce4e7
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-28 10:43:05 -1000

    pending tests

--- test/salutation.spec.js
+++ test/salutation.spec.js
@@ -1,17 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
-const scoreThrows = require ('../index.js');
+const scoreFunction = require ('../index.js');
+
 
 describe('scoreThrows', () => {
 
-    it('should be a function', () => {
-      expect(scoreThrows).to.be.a('function');
-    });
+  it('should be a function', () => {
+    expect(scoreFunction).to.be.a('function');
+  });
 
-    describe('scoring function', () =>{
+    describe('throwing darts', () =>{
 
       it('should return a final score', () => {
-        expect(scoreThrows([1,2,3,4])).to.equal(140);
+        expect(scoreFunction([1,5,11])).to.equal(15);
       });
 
   });
